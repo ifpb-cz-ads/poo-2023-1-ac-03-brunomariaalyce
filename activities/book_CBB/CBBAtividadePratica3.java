d4def70240368c73acc17165b2dabe39e2596a7a
Alyce Barreto <barretomalyce@gmail.com>
2023-08-28 20:41:08 -0300

    CBB Atividade Pratica 3

---
+++ activities/book_CBB/CBBAtividadePratica3.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica3 {
+
+    public static void main(String[] args) {
+        double largura = 3;
+        double comprimento = 10;
+
+        double area = largura * comprimento;
+
+        System.out.println("Largura: " + largura);
+        System.out.println("Comprimento: " + comprimento);
+        System.out.println("Area: " + area);
+    }
+    
+}
