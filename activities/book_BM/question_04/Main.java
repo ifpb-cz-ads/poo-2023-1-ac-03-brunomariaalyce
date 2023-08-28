dcce8f4e942a1b5c303433e892e293fecbbd4a86
Bruno Henrique <brunohenriquex00@gmail.com>
2023-08-28 17:39:30 -0300

    resposta da questao 4 - BM

---
+++ activities/book_BM/question_04/Main.java
@@ -0,0 +1,10 @@
+package activities.book_BM.question_04;
+
+public class Main {
+    public static void main(String[] args) {
+        long long1 = 100;
+        int int1 = 200;
+        int int2 = (int) (int1 + long1);
+        
+    }
+}
